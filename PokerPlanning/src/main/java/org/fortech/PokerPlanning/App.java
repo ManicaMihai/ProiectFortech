5f5d5ab5358fa79e8ddff23cc7dd861bbb0e6ab4
Manica Mihai <manica_mihai_paul@yahoo.com>
2015-02-13 12:08:36 +0200

    Created project

---
+++ PokerPlanning/src/main/java/org/fortech/PokerPlanning/App.java
@@ -0,0 +1,13 @@
+package org.fortech.PokerPlanning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
